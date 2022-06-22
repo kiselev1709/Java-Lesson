be34a474c11fd9017747524a39bbde5f3c002aaf
Your Name <a9119406541@gmail.com>
2022-06-22 17:53:14 +0300

    hw

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountInKopecks = 1000000;
+        int percentInKopecks = 2000;
+        int bonus = amountInKopecks / percentInKopecks;
+        System.out.println("Andrey, hello! Thank you for choosing our airline for your flight. Your bonus is:"+ bonus +" miles");
+
+
+
+
+    }
+}
